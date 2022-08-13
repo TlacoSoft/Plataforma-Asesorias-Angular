1dff75f27366d0d7a317cabffd673377243dd2fa
Naye Mendoza <mendozanayeli2401@gmail.com>
2022-08-13 07:15:41 -0500

    Validaciones con el formulario requtest-a

--- src/app/services/asesorias.service.ts
+++ src/app/services/asesorias.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
+
 })
 export class AsesoriasService {
 
@@ -12,4 +14,18 @@ export class AsesoriasService {
   getAll() {
     return this.http.get("https://asesoriasnode.azurewebsites.net/asesorias/get");
   }
+
+  //Obtener profesores
+  getprof(){
+    return this.http.get("https://asesoriasnode.azurewebsites.net/profesoress/get");
+  }
+
+  getuser(){
+    return this.http.get("https://asesoriasnode.azurewebsites.net/users/get")
+  }
+//agregar una asesoria
+  requesta(form: any){
+    return this.http.post('https://asesoriasnode.azurewebsites.net/asesorias/create',form);
+  }
+
 }
